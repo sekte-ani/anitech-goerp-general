51907593edc968eae45b5919f342f08b8eda20d2
Gswaraw37 <genta.swara@gmail.com>
2025-02-08 20:05:10 +0700

    feat: Menambahkan controller, model, responses, middleware untuk api employee

--- routes/api.go
+++ routes/api.go
@@ -10,13 +10,8 @@ import (
 func InitRoutes(app *gin.Engine){
 	route := app
 	route.Static(appconfig.STATIC_ROUTE, appconfig.STATIC_DIR)
-
-	// AUTH ROUTES
-	// authRoute := route.Group("/api/auth")
-	// authRoute.POST("/login", controllers.Login)
 	
 	// USER ROUTES
 	userRoute := route.Group("/api/employee")
 	userRoute.GET("/", controllers.IndexEmployee)
-	// userRoute.GET("/:id", controllers.ShowEmployee)
 }
