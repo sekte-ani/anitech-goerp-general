ca79b677913bd4cb23391b1ecd4fa923872e7a32
Gswaraw37 <genta.swara@gmail.com>
2025-02-08 20:04:42 +0700

    feat: Menambahkan controller, model, responses, middleware untuk api employee

--- app/models/Product.go
+++ app/models/Product.go
@@ -1,5 +1,17 @@
 package models
 
+import "time"
+
 type Product struct {
-	
+	ID 				uint 		`json:"id"`
+	Name     		string 		`json:"name"`
+	Slug     		string 		`json:"slug"`
+	Description     string 		`json:"description"`
+	Images     		string 		`json:"images"`
+	CreatedAt   	time.Time 	`json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt   	time.Time 	`json:"updated_at" gorm:"autoUpdateTime"`
+}
+
+func (Product) TableName() string {
+	return "products"
 }
